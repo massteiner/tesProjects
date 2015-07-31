e92e7e65325e8438ceec0fb16d9f4f25daddae67
Marcel Steiner <m.steiner84@bluewin.ch>
2015-07-31 15:56:52 +0200

    initial design

---
+++ netCore/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World from Core CLR!");
+        var text = Console.ReadLine();
+        Console.WriteLine($"I read {text}");
+    }
+}
